23d9baae0e5da6e930d1695816b1a99440a8d07d
PATNAMGNANASAI <95867359+PATNAMGNANASAI@users.noreply.github.com>
2021-12-27 19:50:50 +0530

    pandu.c
    developed by Patnam Gnana sai

---
+++ pandu.c
@@ -0,0 +1,17 @@
+Input;
+element-[0],[0]:1
+element-[0],[1]:2
+element-[0],[2]:3
+element-[1],[0]:4
+element-[1],[1]:5
+element-[1],[2]:6
+element-[2],[0]:7
+element-[2],[1]:8
+element-[2],[2]:9
+
+Output;
+The matrix is;
+
+1    2    3
+4    5    6
+7    8    9
